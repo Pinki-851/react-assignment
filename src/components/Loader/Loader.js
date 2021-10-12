d4ad605bf02e843bf8c3eb15682453ffef38dc28
Pinki-851 <Pinkisaroj851@outlook.com>
2021-10-12 13:05:30 +0530

    button added

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,20 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./Loader.css";
-const Loader = () => {
+const Loader = (props) => {
+  console.log(props);
+  const { showError } = props;
+
   return (
-    <div className="loader_container">
-      <div className="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <>
+      {" "}
+      <div className="loader_container">
+        {showError ? (
+          <button>retry</button>
+        ) : (
+          <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    showError: state.cardReducer.showError,
+  };
+};
 
-export default Loader;
+export default connect(mapStateToProps)(Loader);
--- src/store/Reducer/Reducer.js
+++ src/store/Reducer/Reducer.js
@@ -1,10 +1,15 @@
-const initialState = { cardData: [], loadingState: true };
+const initialState = { cardData: [], loadingState: true, showError: false };
 const CardReducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_CARD_DATA":
       return { ...state, cardData: action.payload, loadingState: false };
     case "ERR_CARD_DATA":
-      return { ...state, cardData: action.payload, loadingState: true };
+      return {
+        ...state,
+        cardData: action.payload,
+        loadingState: true,
+        showError: true,
+      };
     default:
       return state;
   }
